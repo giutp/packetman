2e2df7335499f61ae8d1facc729047fbbaccc451
GiuTP <giuliannopinheirotavares@gmail.com>
2026-04-21 19:58:30 -0300

    [add] adicionado ia fantasmas

--- game.c
+++ game.c
@@ -115,3 +115,136 @@ void move_pacman(char map[N][N], pacman_t *pacman, direction_t direction){
         if (pacman->count_mov % 5 == 0) pacman->radius++;
     }
 }
+
+void bind_pellets_files(pellet_t *pellets){
+    // O caminho de onde vai esta os arquivos ainda nao definir, entao pode mudar a atribuicao
+    pellets[0].filepath = "1.txt";
+    pellets[1].filepath = "2.txt";
+    pellets[2].filepath = "3.jpg";
+    pellets[3].filepath = "4.jpg";
+    pellets[4].filepath = "5.mp4";
+    pellets[5].filepath = "6.mp4";
+}
+
+int check_pellets(pacman_t *pacman, pellet_t *pellets){
+    for (int i = 0; i < 6; i++){
+        if (!pellets[i].collected && pacman->position.x == pellets[i].position.x && pacman->position.y == pellets[i].position.y){
+            pellets[i].collected = 1;
+            pacman->pellets++;
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+static int check_wall_free(char map[N][N], coord_t ghost_coord, direction_t new_direction, coord_t *new_coord){
+    switch (new_direction){
+    case UP:
+        ghost_coord.y--;
+        break;
+    
+    case DOWN:
+        ghost_coord.y++;
+        break;
+
+    case LEFT:
+        ghost_coord.x--;
+        break;
+
+    case RIGHT:
+        ghost_coord.x++;
+        break;
+    }
+
+    if (((ghost_coord.y >= 0) && (ghost_coord.y < N)) 
+        && ((ghost_coord.x >= 0) && (ghost_coord.x < N)) 
+        && (map[ghost_coord.y][ghost_coord.x] != 'X')){
+            *new_coord = ghost_coord;
+            return 0;
+        }
+
+    return -1;
+}
+
+void move_ghosts(char map[N][N], ghost_t *ghosts){
+    for (int i = 0; i < 4; i++){
+        coord_t new_cord = ghosts[i].position;
+
+        // Todo fantasma tem um array de prioridade de movimento
+        // TODO: Da para simplificar esse codigo com mais funcao auxiliar (depois faco)
+        switch (ghosts[i].color){
+        // Mão esquerda
+        case RED:{  
+            direction_t direction_relative_priority[4] = {
+                (ghosts[i].curr_direc + LEFT) % 4, 
+                (ghosts[i].curr_direc + UP) % 4, 
+                (ghosts[i].curr_direc + RIGHT) % 4, 
+                (ghosts[i].curr_direc + DOWN) % 4
+            };
+
+            int j = 0;
+            while(j < 4 && check_wall_free(map, ghosts[i].position, direction_relative_priority[j], &new_cord) != 0) j++;
+
+            if (j < 4){
+                ghosts[i].prev_direc = ghosts[i].curr_direc;
+                ghosts[i].curr_direc = direction_relative_priority[j];
+                ghosts[i].position = new_cord;
+            }
+
+            break;
+        }
+        // Mão direita
+        case BLUE:{  
+            direction_t direction_relative_priority[4] = {
+                (ghosts[i].curr_direc + RIGHT) % 4,
+                (ghosts[i].curr_direc + UP) % 4, 
+                (ghosts[i].curr_direc + LEFT) % 4, 
+                (ghosts[i].curr_direc + DOWN) % 4
+            };
+
+            int j = 0;
+            while(j < 4 && check_wall_free(map, ghosts[i].position, direction_relative_priority[j], &new_cord) != 0) j++;
+
+            if (j < 4){
+                ghosts[i].prev_direc = ghosts[i].curr_direc;
+                ghosts[i].curr_direc = direction_relative_priority[j];
+                ghosts[i].position = new_cord;
+            }
+
+            break;
+        }
+        // Alterna entre mão direita e esquerda usando paridade do grid
+        case GREEN:{
+
+            break;
+        }
+
+        // Aleatorio
+        case YELLOW:{
+            direction_t direction_relative_priority[4] = {0, 1, 2, 3};
+            // Embaralha o array de prioridade
+            for (int j = 3; j > 0; j--){
+                int swap = rand()%(j+1);
+                int tmp = direction_relative_priority[j];
+                direction_relative_priority[j] = direction_relative_priority[swap];
+                direction_relative_priority[swap] = tmp;
+            }
+            // Calcula as direcoes relativas com o array embralhado
+            for (int j = 0; j < 4; j++)
+                direction_relative_priority[j] = (ghosts[i].curr_direc + direction_relative_priority[j])%4;
+
+            int j = 0;
+            while(j < 4 && check_wall_free(map, ghosts[i].position, direction_relative_priority[j], &new_cord) != 0) j++;
+
+            if (j < 4){
+                ghosts[i].prev_direc = ghosts[i].curr_direc;
+                ghosts[i].curr_direc = direction_relative_priority[j];
+                ghosts[i].position = new_cord;
+            }
+
+            break;
+            }
+        }
+    }
+}
