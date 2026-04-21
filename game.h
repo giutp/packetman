0e5429850df47e45e6154517ce8a9f7f3fb7674d
GiuTP <giuliannopinheirotavares@gmail.com>
2026-04-21 11:02:13 -0300

    [add] adicionado movimentacao do pacman e init de mov dos fantasmas como aleatorio

--- game.h
+++ game.h
@@ -58,4 +58,6 @@ int read_map(char *filepath, char map[N][N]);
 
 void init_entities(char map[N][N], pacman_t *pacman, ghost_t *ghosts, pellet_t *pellets);
 
+void move_pacman(char map[N][N], pacman_t *pacman, direction_t direction);
+
 #endif
