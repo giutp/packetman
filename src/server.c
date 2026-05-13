2f064a790d952d36b8326875285477e71a1928ec
Vinicius Hasse <hassevini@gmail.com>
2026-05-13 03:05:39 -0300

    [mod] ordem do loop do server

--- src/server.c
+++ src/server.c
@@ -34,13 +34,84 @@ int main(int argc, char **argv){
 
     init_entities(map, &pacman, ghosts, pellets);
 
+    // Inicialização variáveis de rede
     int socket = create_raw_socket(argv[1]);
     kermit_t rcv_msg, send_msg;                                                     // struct de mensagens
     int curr_seq = 0, expected_seq = 0;                                             // sequencia de mensagens
     uint8_t send_buffer[TAM_BUFFER];                                                // buffer de envia mensagem
     uint8_t rcv_buffer[TAM_BUFFER];                                                 // buffer de receber mensagem
 
+    // inicializado com valor para ser ignorado
+    int game_response = -10;
+    FILE *pellet_file;     
+
     while(1){
+        if(game_response >= 1 && game_response <= 6){
+            // Envio do identificador da pastilha
+            create_control_msg(&send_msg, direction, curr_seq);
+            int send_bytes = serialize_msg(&send_msg, send_buffer);
+            send(socket, send_buffer, send_bytes, 0);
+
+            uint8_t file_buffer[MAX_DATA];
+            size_t bytes_read;
+
+            // envio dos dados parciais
+            while ((bytes_read = fread(file_buffer, 1, MAX_DATA, pellet_file)) > 0) {
+                create_data_msg( &send_msg, DADOS, curr_seq, file_buffer, bytes_read);
+                int send_bytes = serialize_msg(&send_msg, send_buffer);
+                send_with_ack(socket, send_buffer, send_bytes, rcv_buffer, &rcv_msg, &curr_seq);
+            }
+        }
+        else if(game_response == -1){
+            create_control_msg(&send_msg, DERROTA, curr_seq);
+            int send_bytes = serialize_msg(&send_msg, send_buffer);
+            send(socket, send_buffer, send_bytes, 0);
+        }
+        else if(game_response == 10){
+            create_control_msg(&send_msg, VITORIA, curr_seq);
+            int send_bytes = serialize_msg(&send_msg, send_buffer);
+            send(socket, send_buffer, send_bytes, 0);
+        }
+
+        // envio do mapa
+
+        // Envio do identificado de visualização
+        create_control_msg(&send_msg, VISUALIZACAO, curr_seq);
+        int send_bytes = serialize_msg(&send_msg, send_buffer);
+        send_with_ack(socket, send_buffer, send_bytes, rcv_buffer, &rcv_msg, &curr_seq);
+
+        // Envio do raio
+        create_control_msg(&send_msg, RAIO, curr_seq);
+        int send_bytes = serialize_msg(&send_msg, send_buffer);
+        send_with_ack(socket, send_buffer, send_bytes, rcv_buffer, &rcv_msg, &curr_seq);
+
+        // Envio da área visível
+        int side = 2 * pacman.radius + 1;
+        int total = side * side;
+
+        uint8_t *submatrix_buffer = malloc(total*sizeof(uint8_t));
+        build_submatrix(map, &pacman, ghosts, pellets, submatrix_buffer);
+       
+        int offset = 0;
+
+        while (offset < total) {
+            int chunk_size = MAX_DATA;
+            if (total - offset < MAX_DATA)
+                chunk_size = total - offset;
+
+            create_data_msg(&send_msg, DADOS, curr_seq, submatrix_buffer + offset, chunk_size);
+            int send_bytes = serialize_msg(&send_msg, send_buffer);
+            send_with_ack(socket, send_buffer, send_bytes, rcv_buffer, &rcv_msg, &curr_seq);
+
+            offset += chunk_size;
+        }
+
+        free(submatrix_buffer);
+
+        create_control_msg(&send_msg, FIM_DA_TRANSMISSAO, curr_seq);
+        int send_bytes = serialize_msg(&send_msg, send_buffer);
+        send_with_ack(socket, send_buffer, send_bytes, rcv_buffer, &rcv_msg, &curr_seq);
+
         int flag_ntw = 0;
         while(1){
 
@@ -90,9 +161,8 @@ int main(int argc, char **argv){
             }
         }
 
-        int game_response = update_world(map, &pacman, ghosts, pellets, direction);
+        game_response = update_world(map, &pacman, ghosts, pellets, direction);
 
-        FILE *pellet_file;
         switch (game_response)
         {
             case 1:
@@ -116,62 +186,6 @@ int main(int argc, char **argv){
             default:
                 break;
         }
-
-        if(game_response >= 1 && game_response <= 6){
-            // Envio do identificador da pastilha
-            create_control_msg(&send_msg, direction, curr_seq);
-            int send_bytes = serialize_msg(&send_msg, send_buffer);
-            send(socket, send_buffer, send_bytes, 0);
-
-            uint8_t file_buffer[MAX_DATA];
-            size_t bytes_read;
-
-            // envio dos dados parciais
-            while ((bytes_read = fread(file_buffer, 1, MAX_DATA, pellet_file)) > 0) {
-                create_data_msg( &send_msg, DADOS, curr_seq, file_buffer, bytes_read);
-                int send_bytes = serialize_msg(&send_msg, send_buffer);
-                send_with_ack(socket, send_buffer, send_bytes, rcv_buffer, &rcv_msg, &curr_seq);
-            }
-        }
-
-        // envio do mapa
-
-        // Envio do identificado de visualização
-        create_control_msg(&send_msg, VISUALIZACAO, curr_seq);
-        int send_bytes = serialize_msg(&send_msg, send_buffer);
-        send_with_ack(socket, send_buffer, send_bytes, rcv_buffer, &rcv_msg, &curr_seq);
-
-        // Envio do raio
-        create_control_msg(&send_msg, RAIO, curr_seq);
-        int send_bytes = serialize_msg(&send_msg, send_buffer);
-        send_with_ack(socket, send_buffer, send_bytes, rcv_buffer, &rcv_msg, &curr_seq);
-
-        // Envio da área visível
-        int side = 2 * pacman.radius + 1;
-        int total = side * side;
-
-        uint8_t *submatrix_buffer = malloc(total*sizeof(uint8_t));
-        build_submatrix(map, &pacman, ghosts, pellets, submatrix_buffer);
-       
-        int offset = 0;
-
-        while (offset < total) {
-            int chunk_size = MAX_DATA;
-            if (total - offset < MAX_DATA)
-                chunk_size = total - offset;
-
-            create_data_msg(&send_msg, DADOS, curr_seq, submatrix_buffer + offset, chunk_size);
-            int send_bytes = serialize_msg(&send_msg, send_buffer);
-            send_with_ack(socket, send_buffer, send_bytes, rcv_buffer, &rcv_msg, &curr_seq);
-
-            offset += chunk_size;
-        }
-
-        free(submatrix_buffer);
-
-        create_control_msg(&send_msg, FIM_DA_TRANSMISSAO, curr_seq);
-        int send_bytes = serialize_msg(&send_msg, send_buffer);
-        send_with_ack(socket, send_buffer, send_bytes, rcv_buffer, &rcv_msg, &curr_seq);
     }
 
     return 0;
