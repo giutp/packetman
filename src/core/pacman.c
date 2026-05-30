e1c9ce3ad5d6a86190398be2a180a17b51099a11
GiuTP <giuliannopinheirotavares@gmail.com>
2026-05-30 12:17:33 -0300

    [fix] corrigido colisao de paredes

--- src/core/pacman.c
+++ src/core/pacman.c
@@ -22,7 +22,7 @@ void move_pacman(char map[N][N], pacman_t *pacman, direction_t direction){
 
     if (((new_coord.y >= 0) && (new_coord.y < N)) 
         && ((new_coord.x >= 0) && (new_coord.x < N)) 
-        && (map[new_coord.y][new_coord.x] != 'X')){
+        && (map[new_coord.y][new_coord.x] != 'x' || map[new_coord.y][new_coord.x] != 'X' )){
         pacman->position.x = new_coord.x;
         pacman->position.y = new_coord.y;
 
