85590fd33b0ca94d739838802ba4506fbf4844cd
GiuTP <giuliannopinheirotavares@gmail.com>
2026-06-03 10:28:58 -0300

    [add] inserido cores

--- src/core/interface.c
+++ src/core/interface.c
@@ -12,6 +12,7 @@ void init_interface(){
     init_pair(5, COLOR_YELLOW, COLOR_BLACK);
     init_pair(6, COLOR_WHITE, COLOR_BLACK);
     init_pair(7, COLOR_CYAN, COLOR_BLACK);
+    init_pair(8, COLOR_BLACK, COLOR_WHITE);
 
     cbreak();
     noecho();
